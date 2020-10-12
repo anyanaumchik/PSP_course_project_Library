e1e1dd495978a6f91271e5a4d95e84386c932bf7
Anya Naumchik <anyanaumchik16@gmail.com>
2020-10-13 01:10:56 +0300

    server add commit

---
+++ kursProjectServer/src/Configs.java
@@ -0,0 +1,7 @@
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "npkti1031";
+    protected String dbName = "library";
+}
